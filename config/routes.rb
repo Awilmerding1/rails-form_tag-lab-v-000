ea8e35107fea427be1fb0f63efc55edd08f735d7
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-18 19:22:24 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index. :new, :create]
   
   get '/students/new', to: 'students#new', as: 'new_student'
   
